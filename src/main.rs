8ae6093a3e7389d15bcd704cd21600e58c63c8eb
Lander Brandt <landerbrandt@gmail.com>
2023-06-01 02:30:20 -0700

    WIP: GameParams parsing

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use eyre::{Result, WrapErr};
 use flate2::read::{DeflateDecoder, ZlibDecoder};
 use memmap::MmapOptions;
 use pkg::PkgFileLoader;
+use serde_json::Map;
+use serde_pickle::DeOptions;
 use std::{
+    convert,
     fs::{self, File, FileType},
-    io::{Cursor, Read},
+    io::{BufWriter, Cursor, Read},
     path::PathBuf,
     sync::Mutex,
     time::Instant,
@@ -115,6 +118,142 @@ fn main() -> Result<()> {
 
     let idx_files = resources.into_inner().unwrap();
     let file_tree = idx::build_file_tree(&idx_files);
+
+    let game_params = file_tree.find("content/GameParams.data")?;
+    let mut game_params_data = Vec::new();
+    game_params.read_file(pkg_loader.as_mut().unwrap(), &mut game_params_data)?;
+    game_params_data.reverse();
+
+    let mut decompressed_data = Cursor::new(Vec::new());
+    let mut decoder = ZlibDecoder::new(Cursor::new(game_params_data));
+    std::io::copy(&mut decoder, &mut decompressed_data)?;
+    decompressed_data.set_position(0);
+
+    let decoded: serde_pickle::Value = serde_pickle::from_reader(
+        &mut decompressed_data,
+        DeOptions::default()
+            .replace_unresolved_globals()
+            .replace_recursive()
+            .decode_strings(),
+    )?;
+
+    fn hashable_pickle_to_json(pickled: serde_pickle::HashableValue) -> serde_json::Value {
+        match pickled {
+            serde_pickle::HashableValue::None => serde_json::Value::Null,
+            serde_pickle::HashableValue::Bool(v) => serde_json::Value::Bool(v),
+            serde_pickle::HashableValue::I64(v) => {
+                serde_json::Value::Number(serde_json::Number::from(v))
+            }
+            serde_pickle::HashableValue::Int(v) => todo!(),
+            serde_pickle::HashableValue::F64(v) => {
+                serde_json::Value::Number(serde_json::Number::from_f64(v).expect("invalid f64"))
+            }
+            serde_pickle::HashableValue::Bytes(v) => serde_json::Value::Array(
+                v.into_iter()
+                    .map(|b| serde_json::Value::Number(serde_json::Number::from(b)))
+                    .collect(),
+            ),
+            serde_pickle::HashableValue::String(v) => serde_json::Value::String(v),
+            serde_pickle::HashableValue::Tuple(v) => {
+                serde_json::Value::Array(v.into_iter().map(hashable_pickle_to_json).collect())
+            }
+            serde_pickle::HashableValue::FrozenSet(v) => {
+                serde_json::Value::Array(v.into_iter().map(hashable_pickle_to_json).collect())
+            }
+        }
+    }
+
+    fn pickle_to_json(pickled: serde_pickle::Value) -> serde_json::Value {
+        match pickled {
+            serde_pickle::Value::None => serde_json::Value::Null,
+            serde_pickle::Value::Bool(v) => serde_json::Value::Bool(v),
+            serde_pickle::Value::I64(v) => serde_json::Value::Number(serde_json::Number::from(v)),
+            serde_pickle::Value::Int(v) => todo!(),
+            serde_pickle::Value::F64(v) => {
+                serde_json::Value::Number(serde_json::Number::from_f64(v).expect("invalid f64"))
+            }
+            serde_pickle::Value::Bytes(v) => serde_json::Value::Array(
+                v.into_iter()
+                    .map(|b| serde_json::Value::Number(serde_json::Number::from(b)))
+                    .collect(),
+            ),
+            serde_pickle::Value::String(v) => serde_json::Value::String(v),
+            serde_pickle::Value::List(v) => {
+                serde_json::Value::Array(v.into_iter().map(pickle_to_json).collect())
+            }
+            serde_pickle::Value::Tuple(v) => {
+                serde_json::Value::Array(v.into_iter().map(pickle_to_json).collect())
+            }
+            serde_pickle::Value::Set(v) => {
+                serde_json::Value::Array(v.into_iter().map(hashable_pickle_to_json).collect())
+            }
+            serde_pickle::Value::FrozenSet(v) => {
+                serde_json::Value::Array(v.into_iter().map(hashable_pickle_to_json).collect())
+            }
+            serde_pickle::Value::Dict(v) => {
+                let mut map = Map::new();
+                for (key, value) in &v {
+                    let converted_key = hashable_pickle_to_json(key.clone());
+                    let string_key = match converted_key {
+                        serde_json::Value::Number(num) => num.to_string(),
+                        serde_json::Value::String(s) => s.to_string(),
+                        other => {
+                            continue;
+                            panic!(
+                                "Unsupported key type: {:?} (original: {:#?}, {:#?})",
+                                other, key, v
+                            );
+                        }
+                    };
+
+                    let converted_value = pickle_to_json(value.clone());
+
+                    map.insert(string_key, converted_value);
+                }
+
+                serde_json::Value::Object(map)
+            }
+        }
+    }
+
+    // match decoded {
+    //     serde_pickle::Value::None => todo!(),
+    //     serde_pickle::Value::Bool(_) => todo!(),
+    //     serde_pickle::Value::I64(_) => todo!(),
+    //     serde_pickle::Value::Int(_) => todo!(),
+    //     serde_pickle::Value::F64(_) => todo!(),
+    //     serde_pickle::Value::Bytes(_) => todo!(),
+    //     serde_pickle::Value::String(_) => todo!(),
+    //     serde_pickle::Value::List(list) => panic!("{}", &list[1]),
+    //     serde_pickle::Value::Tuple(_) => todo!(),
+    //     serde_pickle::Value::Set(_) => todo!(),
+    //     serde_pickle::Value::FrozenSet(_) => todo!(),
+    //     serde_pickle::Value::Dict(_) => todo!(),
+    // };
+
+    // panic!("{:#?}", decoded);
+
+    let converted = if let serde_pickle::Value::List(list) = decoded {
+        pickle_to_json(list.into_iter().next().unwrap())
+    } else {
+        panic!("");
+    };
+    println!(
+        "converted in {} seconds",
+        (Instant::now() - timestamp).as_secs_f32()
+    );
+
+    println!("writing data");
+    let mut f = BufWriter::new(File::create("GameParams.json")?);
+    //let mut data = Vec::new();
+    serde_json::to_writer_pretty(f, &converted)?;
+    //std::fs::write("GameParams.data", &data);
+    println!(
+        "Parsed resources in {} seconds",
+        (Instant::now() - timestamp).as_secs_f32()
+    );
+
+    panic!("");
 
     match &args.command {
         Commands::Extract {
