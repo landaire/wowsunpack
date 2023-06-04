5bb1227baeb08ffb3531b6c4324f4c12bfff6d42
Lander Brandt <landerbrandt@gmail.com>
2023-06-04 00:51:37 -0700

    update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+/// Utilities for interacting with the `content/GameParams.data` file
 pub mod game_params;
+/// Main logic for parsing the game's resource index files
 pub mod idx;
+/// Utilities for helping load and maintain `.pkg` files
 pub mod pkg;
+/// Utilities for assisting with serializing game resource metadata
 pub mod serialization;
