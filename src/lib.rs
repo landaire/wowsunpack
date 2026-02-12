dc8890630ddfe2cb0997a2543a409d92e2b03210
Lander Brandt <landergriffith@gmail.com>
2026-02-12 15:38:45 -0800

    feat: add utility functions for getting game directories/files

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ pub mod error;
 pub mod game_params;
 /// Utilities involving the game's RPC functions -- useful for parsing entity defs and RPC definitions.
 pub mod rpc;
+/// Constants parsed from the game's XML files in `res/gui/data/constants/`
+pub mod game_constants;
 
 #[cfg(feature = "arc")]
 pub type Rc<T> = std::sync::Arc<T>;
